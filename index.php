88e1a84639e29b5255a08ae2be4e4ebb2ed3f607
Ibrahim Hasan <ibrahim_hasan_eng@hotmail.com>
2017-12-28 19:07:24 +0300

    restructured the page using Bootstrap classes and added smooth scroll

--- index.php
+++ index.php
@@ -1,177 +1,164 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="definition" content="Ibrahim Hasan - This is my personal blog.">
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Different Title to Each Page -->
-    <title>Home</title>
-    <!-- Bootstrap Stylesheet CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <!-- fontAwesome for Text Icons -->
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-    <!-- Google Font for Brand Name -->
-    <link href="https://fonts.googleapis.com/css?family=Courgette|Kaushan+Script" rel="stylesheet">
-    <!-- My Custom Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <!-- Animate.css javascript Plugin -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous">
-    </head>
-    <body>
-        <header>
-          <div class="header">
-            <nav class="navbar-transparent navbar navbar-expand-md navbar-dark animated fadeIn">
-              <a class="navbar-brand animated pulse infinite" href="index.php" id="navbar-brand">Ibrahim Hasan</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+  <meta name="definition" content="Ibrahim Hasan - This is my personal blog.">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Different Title to Each Page -->
+  <title>Home</title>
+  <!-- Bootstrap Stylesheet CDN -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <!-- fontAwesome for Text Icons -->
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <!-- Google Font for Brand Name -->
+  <link href="https://fonts.googleapis.com/css?family=Courgette|Kaushan+Script" rel="stylesheet">
+  <!-- My Custom Stylesheet -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!-- Animate.css javascript Plugin -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+  </head>
+  <body data-spy="scroll" data-target="#navbar">
+    <header>
+      <nav id="navbar" class="navbar-transparent navbar navbar-expand-md navbar-dark animated fadeIn">
+        <a class="navbar-brand animated pulse infinite" href="index.php" id="navbar-brand">Ibrahim Hasan</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                  <li id="dropdown" class="nav-item active dropdown">
-                    <a class="nav-link dropdown-toggle" href="#">Home <i id="chevron" class="fa fa-chevron-down" aria-hidden="true"></i></a>
-                    <ul id="home-dropdown" class="dropdown-menu">
-                      <li><a href="#">About Me</a></li>
-                      <li><a href="#">Services</a></li>
-                      <li><a href="#">Projects</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="resume/index.php" >Resume</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="projects/index.php" >Projects</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="contact/index.php" >Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-        </header>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li id="onHome" class="nav-item">
+              <a class="nav-link" href="#home">Home</a>
+            </li>
+            <li id="onAboutMe" class="nav-item">
+              <a class="nav-link" href="#aboutMe">About Me</a>
+            </li>
+            <li id="onServices" class="nav-item">
+              <a class="nav-link" href="#services">Services</a>
+            </li>
+            <li id="onProjects" class="nav-item">
+              <a class="nav-link" href="#projects">Projects</a>
+            </li>
+          </ul>
+        </div><!-- End of Collapse -->
+      </nav>
+    </header>
 
-        <div class="container masthead text-center">
-          <div class="row">
-            <div class="col-lg-12 center">
-              <h1 class="display-3">Ibrahim Hasan</h1>
+    <section id="home" class="header masthead text-center">
+      <div class="row">
+        <div class="col-lg-12 center">
+          <h1 class="display-3">Ibrahim Hasan</h1>
+          <hr>
+          <p class="lead">
+            Thank you for visiting my website. Please continue your journey.
+            I promise you, you will not regret it!</p>
+          <br>
+          <a class="btn btn-outline-info btn-about" href="#aboutMe">Want to know me more?</a>
+        </div><!-- End of Column -->
+      </div><!-- End of Row -->
+    </section><!-- End of Section -->
+
+      <section id="aboutMe" class="about-me d-flex align-items-center py-5 py-lg-0">
+        <div class="container">
+          <div class="row justify-content-center align-items-stretch">
+            <div class="col-lg-4 col-6 main-image pb-3">
+                <img class="img-fluid img-thumbnail d-lg-block d-none" src="img/ibrahimhasan.JPG" alt="Personal Photo">
+                <img class="img-fluid rounded-circle img-thumbnail d-lg-none d-block" src="img/ibrahimhasan-rounded.JPG" alt="Personal Photo">
+            </div>
+            <div class="col-lg-7 col-12">
+              <h1 class="display-3">Who am I?</h1>
+              <p class="lead">Here is some background information about me: I am Ibrahim from Syria.
+              I am a mechatronics engineer, graduated in 2015.
+              I have an ambition, and in order to fulfill it, I have to work hard, think smart,
+              and improve myself. My mother tongue is Arabic, and I am fluent in English.
+              I am an excellent computer user. Recently, I have become interested in web development;
+              in fact, this website is my first project as a front-end web developer, and it was designed
+              using HTML5, CSS3, jQuery Library and some PHP. Presently, my brief knowledge about back-end
+              web development needs to be broadened, thus I am learning PHP.</p>
               <hr>
-              <p class="lead">
-                Thank you for visiting my website. Please continue your journey.
-                I promise you, you will not regret it!</p>
-              <br>
-              <a class="btn btn-outline-info" href="#about">Want to know me more?</a>
-            </div>
-          </div>
-        </div>
-
-          <section class="about-me">
-            <div class="container">
-              <div class="row justify-content-center">
-                <div class="col-lg-4 col-md-8 main-image">
-                    <a href="" title="It's me!"><img src="img/ibrahimhasan.JPG" alt="Personal Photo"/></a>
-                </div>
-                <div class="col-lg-7 col-md-8-nowrap">
-                  <h1 class="display-3">Who am I?</h1>
-                  <p class="lead">Here is some background information about me: I am Ibrahim from Syria.
-                  I am a mechatronics engineer, graduated in 2015.
-                  I have an ambition, and in order to fulfill it, I have to work hard, think smart,
-                  and improve myself. My mother tongue is Arabic, and I am fluent in English.
-                  I am an excellent computer user. Recently, I have become interested in web development;
-                  in fact, this website is my first project as a front-end web developer, and it was designed
-                  using HTML5, CSS3, jQuery Library and some PHP. Presently, my brief knowledge about back-end
-                  web development needs to be broadened, thus I am learning PHP.</p>
-                  <hr>
-                  <p>Learn more about
-                    <a class="btn btn-outline-info" href="#" role="button">My Resume</a>
-                    or check
-                    <a class="btn btn-outline-warning" href="#" role="button">My Projects</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </section>
-          <section class="services section feature-box">
-            <div class="container">
-              <!--Section heading-->
-              <h1 class="section-heading pt-4">Why is it so great?</h1>
-              <!--Section description-->
-              <p class="section-description lead grey-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-
-              <!--Grid row-->
-              <div class="row">
-
-                  <!--Grid column-->
-                  <div class="col-lg-4">
-                    <div class="service-heading">
-                      <i class="fa fa-area-chart red-text"></i>
-                      <h5 class="feature-title">Analytics</h5>
-                    </div>
-                      <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti hic.</p>
-                  </div>
-                  <!--Grid column-->
-
-                  <!--Grid column-->
-                  <div class="col-lg-4">
-                    <div class="service-heading">
-                      <i class="fa fa-book cyan-text"></i>
-                      <h5 class="feature-title">Tutorials</h5>
-                    </div>
-                      <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti hic.</p>
-                  </div>
-                  <!--Grid column-->
-
-                  <!--Grid column-->
-                  <div class="col-lg-4">
-                    <div class="service-heading">
-                      <i class="fa fa-coffee orange-text"></i>
-                      <h5 class="feature-title">Relax</h5>
-                    </div>
-                      <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti hic.</p>
-                  </div>
-                  <!--Grid column-->
-
-              </div>
-              <!--Grid row-->
-            </div>
-          </section>
-          <section class="projects">
-            <div class="container">
-
-            </div>
-          </section>
-          <!--Footer-->
-        <footer class="footer">
-          <div class="socials container">
-            <div class="socials">
-              <ul>
-                <!--Facebook-->
-                <li class="list-item"><a class="facebook" href="https://www.facebook.com/ibrahimhasan1990" target="_blank"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a></li>
-                <!--Skype-->
-                <li class="list-item"><a class="skype" href="tel: (+90) 531 959 1332"><i class="fa fa-skype fa-2x" aria-hidden="true"></i></a></li>
-                <!--Linkedin-->
-                <li class="list-item"><a class="linkedin" href="https://tr.linkedin.com/in/ibrahim-hasan-9a0b24121" target="_blank"><i class="fa fa-linkedin-square fa-2x"> </i></a></li>
-                <!--Email-->
-                <li class="list-item"><a class="email" href="mailto: ibrahim_hasan_eng@hotmail.com"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a></li>
-                <!--Phone Number-->
-                <li class="list-item"><a class="phone" href="tel: (+90) 531 959 1332"><i class="fa fa-phone-square fa-2x" aria-hidden="true"></i></a></li>
-              </ul>
-            </div>
-          </div>
-          <!--Copyright-->
-          <div class="footer-copyright">
-              <p class="copyright text-center">
-                Copyright &copy; 2018 by Ibrahim Hasan. All Rights Reserved.
+              <p>Learn more about
+                <a class="btn btn-outline-warning btn-projects" href="#" role="button">My Projects</a>
               </p>
+            </div><!-- End of Column -->
+          </div><!-- End of Row -->
+        </div><!-- End of Container -->
+      </section>
+      <section id="services" class="services feature-box d-flex align-items-center py-5 py-lg-0">
+        <div class="container">
+          <h1 class="section-heading">Why is it so great?</h1>
+          <p class="section-description lead grey-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <!--Grid row-->
+          <div class="row">
+              <!--Grid column-->
+              <div class="col-lg-4">
+                <div class="service-heading">
+                  <i class="fa fa-area-chart red-text"></i>
+                  <h5 class="feature-title">Analytics</h5>
+                </div>
+                  <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti hic.</p>
+              </div>
+              <!--Grid column-->
+
+              <!--Grid column-->
+              <div class="col-lg-4">
+                <div class="service-heading">
+                  <i class="fa fa-book cyan-text"></i>
+                  <h5 class="feature-title">Tutorials</h5>
+                </div>
+                  <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti hic.</p>
+              </div>
+              <!--Grid column-->
+
+              <!--Grid column-->
+              <div class="col-lg-4">
+                <div class="service-heading">
+                  <i class="fa fa-coffee orange-text"></i>
+                  <h5 class="feature-title">Relax</h5>
+                </div>
+                  <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti hic.</p>
+              </div>
+              <!--Grid column-->
           </div>
-        </footer>
-        <!--/Footer-->
-      <!-- Header Shrinks On Scroll -->
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-      <script type="text/javascript" src="js/js.js"></script>
-    </body>
+          <!--Grid row-->
+        </div><!-- End of Container -->
+      </section>
+      <section id="projects" class="projects">
+        <div class="container">
+
+        </div>
+      </section>
+      <!--Footer-->
+      <footer class="footer">
+        <div class="socials container">
+          <div class="socials">
+            <ul>
+              <!--Facebook-->
+              <li class="list-item"><a class="facebook" href="https://www.facebook.com/ibrahimhasan1990" target="_blank"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a></li>
+              <!--Skype-->
+              <li class="list-item"><a class="skype" href="tel: (+90) 531 959 1332"><i class="fa fa-skype fa-2x" aria-hidden="true"></i></a></li>
+              <!--Linkedin-->
+              <li class="list-item"><a class="linkedin" href="https://tr.linkedin.com/in/ibrahim-hasan-9a0b24121" target="_blank"><i class="fa fa-linkedin-square fa-2x"> </i></a></li>
+              <!--Email-->
+              <li class="list-item"><a class="email" href="mailto: ibrahim_hasan_eng@hotmail.com"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a></li>
+              <!--Phone Number-->
+              <li class="list-item"><a class="phone" href="tel: (+90) 531 959 1332"><i class="fa fa-phone-square fa-2x" aria-hidden="true"></i></a></li>
+            </ul>
+          </div><!-- End of Socials -->
+        </div><!-- End of Container -->
+        <!--Copyright-->
+        <div class="footer-copyright">
+            <p class="copyright text-center">
+              Copyright &copy; 2018 by Ibrahim Hasan. All Rights Reserved.
+            </p>
+        </div><!-- End of Copyright -->
+      </footer>
+      <!--/Footer-->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.smooth-scroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,73 @@
+$('document').ready(function() {
+
+  var document = $(window);
+  var navbar = $('.navbar');
+
+  var scrollValue = $(document).scrollTop();
+
+  var target;
+
+  sectionHeight();
+  navOnScroll();
+
+document.scroll(
+  function() {
+    navOnScroll();
+  });
+
+// Smooth Scroll
+
+  $('.nav-link').on('click', function() {
+
+    switch( $(this).text() ) {
+      case 'About Me':
+          target = '#aboutMe';
+          break;
+      case 'Services':
+          target = '#services';
+          break;
+      case 'Projects':
+          target = '#projects';
+          break;
+      default:
+          target = '#home';
+    }
+
+    $.smoothScroll( {scrollTarget: target} );
+  });
+
+  $('.btn-about').on('click', function() {
+          target = '#aboutMe';
+          $.smoothScroll( {scrollTarget: target} );
+    });
+
+  $('.btn-projects').on('click', function() {
+          target = '#projects';
+          $.smoothScroll( {scrollTarget: target} );
+    });
+
+  function sectionHeight() {
+    "use strict";
+    $("section").height($(window).height());
+  }
+
+
+  function navOnScroll() {
+    if(document.scrollTop() > 100) {
+
+      navbar.addClass('bg-dark fixed');
+      navbar.removeClass('navbar-transparent');
+
+      navbar.addClass('fadeInDown');
+
+    } else {
+
+      navbar.addClass('navbar-transparent');
+      navbar.removeClass('bg-dark fixed');
+
+      navbar.removeClass('fadeInDown');
+
+    }
+  }
+
+});
